641b83e12ad1934204c93dfabd285bf6712655c6
David Stewart <david.stewart@Civica.co.uk>
2019-03-26 13:11:44 +0000

    initial

---
+++ AutoFixtureDemo/CalculatorTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace AutoFixtureDemo.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Traditional()
+        {
+
+        }
+    }
+}
---
+++ DemoCode/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoCode
+{
+    public class Calculator
+    {
+        public int Value { get; set; }
+
+        public void Subtract(int number)
+        {
+            Value -= number;
+        }
+    }
+}
