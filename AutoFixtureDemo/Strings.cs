802f16c2006d3f7bb2f50f87a57acb209cacd852
David Stewart <david.stewart@civica.co.uk>
2019-03-26 13:40:14 +0000

    strings

---
+++ AutoFixtureDemo/Strings.cs
@@ -0,0 +1,32 @@
+﻿using AutoFixture;
+using AutoFixtureDemo.Code;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AutoFixtureDemo.Tests
+{
+    public class Strings
+    {
+        [Fact]
+        public void BasicString()
+        {
+            var fixture = new Fixture();
+            var sut = new NameJoiner();
+
+            var firstname = fixture.Create<string>();
+            var lastname = fixture.Create<string>();
+
+            var result = sut.Join(firstname, lastname);
+            Assert.Equal(firstname + " " + lastname, result);
+        }
+
+        [Fact]
+        public void BasicChar()
+        {
+            var fixture = new Fixture();
+            var anonChar = fixture.Create<char>();
+        }
+    }
+}
---
+++ DemoCode/NameJoiner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoFixtureDemo.Code
+{
+    public class NameJoiner
+    {
+        public string Join(string firstname, string lastname)
+        {
+            return firstname + " " + lastname;
+        }
+    }
+}
