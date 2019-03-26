cc1893e042ffc38212574d1890aeced534d15d28
David Stewart <david.stewart@civica.co.uk>
2019-03-26 13:22:26 +0000

    simple first autofixture tests using int

--- AutoFixtureDemo/CalculatorTests.cs
+++ AutoFixtureDemo/CalculatorTests.cs
@@ -1,3 +1,5 @@
+using AutoFixture;
+using DemoCode;
 using Xunit;
 
 namespace AutoFixtureDemo.Tests
@@ -7,7 +9,43 @@ namespace AutoFixtureDemo.Tests
         [Fact]
         public void Traditional()
         {
+            //arrange
+            var sut = new Calculator();
 
+            //act
+            sut.Subtract(1);
+
+            //assert
+            Assert.True(sut.Value < 0);
+        }
+
+        [Fact]
+        public void Manual_Anonymous_Data()
+        {
+            //arrange
+            var sut = new Calculator();
+            var anonymousNumber = 394;
+
+            //act
+            sut.Subtract(anonymousNumber);
+
+            //assert
+            Assert.True(sut.Value < 0);
+        }
+
+        [Fact]
+        public void AutoFixture_Anonymous_Data()
+        {
+            //arrange
+            var sut = new Calculator();
+            var fixture = new Fixture();
+
+            //act
+            sut.Subtract(fixture.Create<int>());    // creating an anonymous int
+                                                    // int is passed as value to subtract
+
+            //assert
+            Assert.True(sut.Value < 0);
         }
     }
 }
